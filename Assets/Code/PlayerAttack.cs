e6d09cdb6241abc710b338e2503a23d6290b7fea
jx1279 <jx1279@nyu.edu>
2022-10-07 01:50:57 -0400

    Got rid of commented code

--- Assets/Code/PlayerAttack.cs
+++ Assets/Code/PlayerAttack.cs
@@ -6,14 +6,12 @@ using UnityEngine.AI;
 
 public class PlayerAttack : MonoBehaviour
 {
-    //NavMeshAgent _agent;
     Camera mainCam;
     public GameObject bulletPrefab;
     int bulletForce = 900;
     // Start is called before the first frame update
     void Start()
     {
-        //_agent = GetComponent<NavMeshAgent>();
         mainCam = Camera.main;
     }
 
@@ -21,12 +19,7 @@ public class PlayerAttack : MonoBehaviour
     void Update()
     {
         
-        /*if (Input.GetMouseButtonDown(0)) {
-            RaycastHit hit;
-            if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 200)) {
-                _agent.SetDestination(hit.point);
-            }
-        }*/
+        
         if (Input.GetMouseButtonDown(1)) {
             RaycastHit hit;
             if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 200)) {
