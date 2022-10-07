9abb40e1f81521c629f8f8abc158ebab2acc080e
jx1279 <jx1279@nyu.edu>
2022-10-07 03:34:54 -0400

    Allowed bullet to temporarily stop enemies in their tracks

--- Assets/Code/EnemyMove.cs
+++ Assets/Code/EnemyMove.cs
@@ -7,16 +7,27 @@ public class EnemyMove : MonoBehaviour
 {
     private GameObject player;
     private NavMeshAgent _agent;
+    private bool paused;
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         player=GameObject.FindGameObjectWithTag("Player");
         StartCoroutine(LookForPlayer());
+        paused = false;
+    }
+    public void stopMovement() {
+        Debug.Log("activated");
+        paused = true;
     }
     IEnumerator LookForPlayer()
     {
         while (true)
         {
+            if (paused) {
+                _agent.ResetPath();
+                yield return new WaitForSeconds(1.0f);
+                paused = false;
+            }
             yield return new WaitForSeconds(.5f);
             _agent.SetDestination(player.transform.position);
         }
