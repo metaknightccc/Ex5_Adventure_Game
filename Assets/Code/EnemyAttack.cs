978c970942b7939f8538dfe6a22dff6593a77396
jx1279 <jx1279@nyu.edu>
2022-10-07 00:56:17 -0400

    Updated Attack

---
+++ Assets/Code/EnemyAttack.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float AttackType = 1;
+    private GameObject player;
+    private UnityEngine.AI.NavMeshAgent _agent;
+
+    public HealthSystem health;
+
+    void Start() {
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Hit");
+        if (other.CompareTag("Player")) {
+            health.TakeDamage(1);
+            Debug.Log("Damaged");
+            //damage player
+        }
+        if (other.CompareTag("Bullet")) {
+            Destroy(gameObject);
+        }
+    }
+}
