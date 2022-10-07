7666db56ec08b1457accb402aa8780f55696daa5
jx1279 <jx1279@nyu.edu>
2022-10-07 01:25:10 -0400

    Updated Enemy Attack

--- Assets/Code/EnemyAttack.cs
+++ Assets/Code/EnemyAttack.cs
@@ -7,6 +7,7 @@ public class EnemyAttack : MonoBehaviour
     public float AttackType = 1;
     private GameObject player;
     private UnityEngine.AI.NavMeshAgent _agent;
+    public float speed 20;
 
     public HealthSystem health;
 
@@ -20,6 +21,11 @@ public class EnemyAttack : MonoBehaviour
             health.TakeDamage(1);
             Debug.Log("Damaged");
             //damage player
+        }
+        if (other.CompareTag("Bullet")) {
+            Vector3 direction = (transform.position - other.transform.position).normalized;
+            gameObject.GetComponent<Rigidbody>().AddForce (direction * speed);
+            Destroy(other.gameObject);
         }
     }
 }
