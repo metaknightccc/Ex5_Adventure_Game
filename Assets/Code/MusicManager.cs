3e1acee11e1ffdcffe00ffb08b94399c1db4fb4d
metaknightccc <sunyifanc@126.com>
2022-10-14 14:50:03 -0400

    Add bgm

---
+++ Assets/Code/MusicManager.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (FindObjectsOfType<MusicManager>().Length>1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
